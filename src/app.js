6b0ad61c801d310848f1c2d3f4e5353a2ed19d33
Jano <janoo.pereira@gmail.com>
2024-11-15 11:56:46 -0300

    First push

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express =require('express');
+
+const app = express();
+
+const path = require('path');
+
+app.set('view engine','ejs');
+app.set('views',path.resolve(__dirname,'./views'))
+app.use(express.static('./public'))
+
+// Para capturar el body
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// Express-session
+const session = require('express-session');
+app.use(session({
+    secret: "Conf middleware global session",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Cookie-parser
+const cookieParser =require('cookie-parser');
+app.use(cookieParser());
+
+// Mehtod-override --> Para usar put y delete (?_method=...)
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+// Rutas
+const mainRouter = require('./routes/mainRouter.js');
+app.use('/',mainRouter);
+
+
+// Correr el servidor
+
+const PORT = process.env.PORT || 3500;
+
+app.listen(PORT,()=>{
+    console.log(" 🚀 Se levanto proyecto en htpp://localhost:"+PORT)
+})
+
