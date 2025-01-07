f5b199e1ee555c1e439eae446dabac674b828168
joaquin <joaco.cataldo3@gmail.com>
2025-01-07 11:36:08 -0300

    creating and getting variations

--- src/routes/api/apiVariationRouter.js
+++ src/routes/api/apiVariationRouter.js
@@ -3,7 +3,8 @@ const router = express.Router();
 import apiVariationsController from '../../controllers/api/apiVariationsController.js';
 
 router.get('/', apiVariationsController.handleGetVariation);
-router.put('/:variationId', apiVariationsController.handleUpdateVariation)
+router.post('/:productId', apiVariationsController.handleCreateVariation);
+router.put('/:variationId', apiVariationsController.handleUpdateVariation);
 router.delete('/', apiVariationsController.handleDeleteVariation);
 
 export default router;
