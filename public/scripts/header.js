4c36e3ade10cca3443929bbb2c82d084404a4cf4
joaquin <joaco.cataldo3@gmail.com>
2025-01-10 14:48:24 -0300

    changes

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -17,20 +17,54 @@ window.addEventListener('DOMContentLoaded', () => {
     scrollToTop()
     if(SCREEN_WIDTH < 720){
         checkForNavbarClicks();
-        checkForShopDropdownClicks();
+        checkForMobileShopDropdownClicks();
+    } else if (SCREEN_WIDTH < 1024) {
+        checkForDesktopShopDropdownClicks()
     }
-    checkForUserIconClicks();
-    // renderFormAndButton();
-    // renderFormAndButton();
+    if(SCREEN_WIDTH >= 720 && SCREEN_WIDTH < 1024){
+        checkForWindowClicksForDropdowns();
+    }
     paintUserIconOrLetter();
     if(userLogged){
         checkForUserLoggedModalClicks();
         checkForUserLoggedModalClose();
-        // checkForUserAnchorsClicks();
     } else {
         checkForUserIconClicks();
     }
 })
+
+const checkForDesktopShopDropdownClicks = () => {
+    const desktopShopItem = document.querySelector('.desktop-shop-item-text-container');
+    const desktopShopDropdown = document.querySelector('.desktop-shop-dropdown');
+    desktopShopItem.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(userLogged){
+            const userInitialsDropdown = document.querySelector('.logged-user-modal');
+            if(userInitialsDropdown.classList.contains('logged-user-modal-active')){
+                userInitialsDropdown.classList.remove('logged-user-modal-active')
+            }
+        }
+        desktopShopDropdown.classList.add('desktop-shop-dropdown-active');
+    })
+}
+
+const checkForWindowClicksForDropdowns = () => {
+    document.addEventListener('click', function(e) {
+        const clickedElement = e.target;
+        if(!clickedElement.classList.contains('logged-user-modal') && !clickedElement.classList.contains('user-initials') && !clickedElement.classList.contains('.shop-a')){
+            if(userLogged){
+                const userLoggedModal = document.querySelector('.logged-user-modal');
+                if(userLoggedModal.classList.contains('logged-user-modal-active')){
+                    userLoggedModal.classList.remove('logged-user-modal-active')
+                }
+            }
+            const shopDropdown = document.querySelector('.desktop-shop-dropdown');
+            if(shopDropdown.classList.contains('desktop-shop-dropdown-active')){
+                shopDropdown.classList.remove('desktop-shop-dropdown-active')
+            }
+        }
+    });
+}
 
 const checkForUserLoggedModalClose = () => {
     const closeBtn = document.querySelector('.close-user-logged-modal');
@@ -47,13 +81,19 @@ const checkForUserLoggedModalClose = () => {
 const checkForUserLoggedModalClicks = () => {
     const userInitials = document.querySelector('.user-initials');
     const userLoggedModal = document.querySelector('.logged-user-modal');
-    userInitials.addEventListener('click', () => {
-        userLoggedModal.classList.add('logged-user-modal-active')
-        if(SCREEN_WIDTH < 720){
-            toggleBodyScrollableBehavior();
-            toggleOverlay();
-        }
-    })
+    const shopDropdown = document.querySelector('.desktop-shop-dropdown');
+    if(SCREEN_WIDTH < 1024) {
+        userInitials.addEventListener('click', () => {
+            if(shopDropdown.classList.contains('desktop-shop-dropdown-active')){
+                shopDropdown.classList.remove('desktop-shop-dropdown-active');
+            }
+            userLoggedModal.classList.add('logged-user-modal-active');
+            if(SCREEN_WIDTH < 720){
+                toggleBodyScrollableBehavior();
+                toggleOverlay();
+            }
+        })
+    }
 }
 
 const checkForNavbarClicks = () => {
@@ -79,7 +119,7 @@ const toggleNavbarMenu = () => {
 };
 
 
-const checkForShopDropdownClicks = () => {
+const checkForMobileShopDropdownClicks = () => {
     const mobileShopDropdownTrigger = document.querySelector('.shop-dropdown-trigger');
     mobileShopDropdownTrigger.addEventListener('click', (e) => {
         toggleShopDropdown();
@@ -210,9 +250,7 @@ const paintUserIconOrLetter = () => {
         const lastNameLetter = userLogged.last_name.split('')[0]
         const userInitialsContainer = document.querySelector('.user-initials-container');
         const userInitialsElement = userInitialsContainer.querySelector('span');
-        console.log(userInitialsElement)
         userInitialsElement.textContent = firstNameLetter + lastNameLetter;
-        console.log(userInitialsElement)
         userInitialsContainer.classList.toggle('hidden');
     } else {
         const userIconElement = document.querySelector('.user-icon');
