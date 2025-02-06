ad90c12cd16e92b5e1f691016541e0a53029e088
joaquincataldo3 <jqncataldo@gmail.com>
2025-02-06 16:27:48 -0300

    mp

---
+++ src/utils/helpers/mercadoPago.js
@@ -0,0 +1,8 @@
+const mp = new MercadoPago('YOUR_PUBLIC_KEY', {
+    locale: 'es-AR'
+});
+
+const button = document.querySelector('.submit-form-btn');
+button.addEventListener('click', async () => {
+
+})
