69090715be54c9109113bef41771f4e7cf617804
joaquincataldo3 <jqncataldo@gmail.com>
2025-03-11 14:18:33 -0300

    adding order to tacos

--- src/utils/staticDB/tacos.js
+++ src/utils/staticDB/tacos.js
@@ -1,22 +1,37 @@
 export default [
     {
         id: 1,
-        name: '9 cm',
+        name: '2 cm',
         category_id: 1
     },
     {
         id: 2,
-        name: '8.5 cm',
+        name: '3 cm',
         category_id: 1
     },
     {
         id: 3,
-        name: '7.5 cm',
+        name: '4 cm',
         category_id: 1
     },
     {
+        id: 8,
+        category_id: 1,
+        name: '6.5 cm'
+    },
+    {
+        id: 9,
+        category_id: 1,
+        name: '7.5 cm'
+    },
+    {
+        id: 10,
+        category_id: 1,
+        name: '8.5 cm'
+    },
+    {
         id: 4,
-        name: '6.5 cm',
+        name: '9 cm',
         category_id: 1
     },
     {
@@ -34,19 +49,5 @@ export default [
         category_id: 2,
         name: 'Goma'
     },
-    {
-        id: 8,
-        category_id: 1,
-        name: '2 cm'
-    },
-    {
-        id: 9,
-        category_id: 1,
-        name: '3 cm'
-    },
-    {
-        id: 10,
-        category_id: 1,
-        name: '4 cm'
-    }
+    
 ]
