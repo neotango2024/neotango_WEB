4bc7c71a2afc1f2e445a00e156697236056fc4f8
Jano <janoo.pereira@gmail.com>
2025-03-17 08:31:58 -0300

    Things

--- public/scripts/componentRenderer.js
+++ public/scripts/componentRenderer.js
@@ -1431,6 +1431,7 @@ export function destroyExistingModal() {
 export function closeModal() {
   $(".ui.modal").modal("hide");
 }
+
 export function userInfoComponent(props) {
   const { first_name, last_name, email } = props;
 
@@ -1864,8 +1865,9 @@ export async function createProductModal(product = undefined) {
         label: "Imagenes",
         name: "product-image",
         allowedExtensions: ["jpg", "jpeg", "png", "webp"],
-        containerClassName: "input-file-container hidden",
+        containerClassName: "input-file-container hidden required",
         // multiple: true,
+        required: true
       },
       {
         type: "field",
